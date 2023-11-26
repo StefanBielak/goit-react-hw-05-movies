3ffe76cb99bfe976d42ea4a8c5e7aa8120ee610b
Stefan Bielak <117030658+StefanBielak@users.noreply.github.com>
2023-11-26 20:50:12 +0100

    components&css_module-dane

--- src/components/movies/movies.jsx
+++ src/components/movies/movies.jsx
@@ -1,32 +1,51 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import movieApi from '../services/movieapi';
+import styles from './movies.module.css'; // Załóż, że styles zawiera odpowiednie style CSS
 
 const Movies = () => {
-  const [trendingMovies, setTrendingMovies] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const navigate = useNavigate();
 
-  useEffect(() => {
-    const fetchTrendingMovies = async () => {
-      try {
-        const data = await movieApi.getTrendingMovies();
-        setTrendingMovies(data.results);
-      } catch (error) {
-        console.error('Error fetching trending movies:', error);
-      }
-    };
+  const handleSearch = async () => {
+    try {
+      const response = await movieApi.searchMovies(searchQuery);
+      setSearchResults(response.results);
+    } catch (error) {
+      console.error('Error searching movies:', error);
+    }
+  };
 
-    fetchTrendingMovies();
-  }, []);
+  const handleMovieClick = (movieId) => {
+    // Przenoszenie użytkownika do strony szczegółów filmu po kliknięciu
+    navigate(`/movies/${movieId}`);
+  };
 
   return (
-    <div>
-      <h2>Trending Movies</h2>
-      <ul>
-        {trendingMovies.map((movie) => (
-          <li key={movie.id}>{movie.title}</li>
+    <div className={styles.moviesContainer}>
+      <div className={styles.searchContainer}>
+        <input
+          type="text"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          placeholder="Search movies..."
+          className={styles.searchInput}
+        />
+        <button onClick={handleSearch} className={styles.searchButton}>
+          Search
+        </button>
+      </div>
+      <div className={styles.searchResults}>
+        {searchResults.map((movie) => (
+          <div key={movie.id} className={styles.movieResult} onClick={() => handleMovieClick(movie.id)}>
+            <img src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} alt={movie.title} className={styles.movieImage} />
+            <p className={styles.movieTitle}>{movie.title}</p>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
 
-export default Movies
+export default Movies;
